f9daec97eee9ed10586c9728d686c031cba47d32
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 18:36:16 +0200

    Add files via upload

---
+++ homework8b/src/homework8b/Homework8b.java
@@ -0,0 +1,127 @@
+package homework8b;
+
+
+import java.util.Scanner;
+
+public class Homework8b {
+
+    public static void main(String[] args) {
+
+        Scanner first = new Scanner(System.in);
+        System.out.println("type in num max 10 and hit enter");
+        int n = first.nextInt();
+       
+        if (n==1){
+            int x = first.nextInt();
+            int w = x;
+            System.out.println("total sum "+w);
+            System.out.println("average "+w);
+        }
+        else if (n==2){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int w = x+y;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/2);
+        } 
+        else if (n==3){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int w = x+y+v;
+            System.out.println("total sum "+w);
+            System.out.println("average "+ w/3);
+        }
+        else if (n==4){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int w = x+y+v+z;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/4);
+        }
+        else if (n==5){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int w = x+y+v+z+c;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/5);
+        }
+        else if (n==6){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int g = first.nextInt();
+            int w = x+y+v+z+c+g;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/6);
+        }
+        else if (n==7){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int g = first.nextInt();
+            int q = first.nextInt();
+            int w = x+y+v+z+c+g+q;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/7);
+        }
+        else if (n==8){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int g = first.nextInt();
+            int q = first.nextInt();
+            int h = first.nextInt();
+            int w = x+y+v+z+c+g+q+h;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/8);
+        }
+        else if (n==9){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int g = first.nextInt();
+            int q = first.nextInt();
+            int h = first.nextInt();
+            int u = first.nextInt();
+            int w = x+y+v+z+c+g+q+h+u;
+            System.out.println("total sum "+ w);
+            System.out.println("average "+ w/9);
+        }
+        else if (n==10){
+            int x = first.nextInt();
+            int y = first.nextInt();
+            int v = first.nextInt();
+            int z = first.nextInt();
+            int c = first.nextInt();
+            int g = first.nextInt();
+            int q = first.nextInt();
+            int h = first.nextInt();
+            int u = first.nextInt();
+            int o = first.nextInt();
+            int w = x+y+v+z+c+g+q+h+u+o;
+            System.out.println("total sum "+ w);
+            System.out.println("average"+ w/10);
+        }
+        
+        
+        
+        
+    }
+} 
+/*
+   int[] array = new int[1];
+*/
